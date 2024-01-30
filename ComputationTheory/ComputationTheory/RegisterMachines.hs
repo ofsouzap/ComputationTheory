f07d9c0a8897ee401abaa8aff00e70a90262ce1e
Ofsouzap <ofsouzap@gmail.com>
2024-01-30 17:47:17 +0000

    Revert "Start register machine"
    This reverts commit 682cc86eb61aaede8380b7e83e23af415e967ca6.

--- ComputationTheory/ComputationTheory/RegisterMachines.hs
+++ ComputationTheory/ComputationTheory/RegisterMachines.hs
@@ -1,47 +1,21 @@
 module ComputationTheory.RegisterMachines
   (  ) where
 
--- Typeclasses
-
-class Enum l => InstructionLabel l
-
-class (Eq a, Bounded a) => MemLoc a
-
--- Data
-
-data Instruction l r =
+data Instruction l =
     Halt
-  | Inc (r, l)
-  | Dec (r, l, l)
+  | Inc l
+  | Dec (l, l)
 
-newtype InstructionLabel l => Program l r = Program (l -> Instruction l r)
+newtype Program l = Program (l -> Instruction l)
 
-type Register = Integer
+newtype Register = Register Integer
 
-regBaseValue :: Register -> Bool
-regBaseValue = (==) 0
+newtype Memory r = Memory (r -> Register)
 
-newtype MemLoc r => Memory r = Memory (r -> Register)
+newtype Config l r = Config (l, Memory r)
 
-memGet :: MemLoc r => r -> Memory r -> Register
-memGet x (Memory f) = f x
+step :: Config l r -> Instruction l -> Either (Instruction l) (Config l r)
+step = undefined -- TODO
 
-memInc :: MemLoc r => r -> Memory r -> Memory r
-memInc r old = Memory (\ x ->
-  if x == r
-  then memGet x old + 1
-  else memGet x old )
-
-memDec :: MemLoc r => r -> Memory r -> (Memory r, Bool)
-memDec r old = undefined -- TODO
-
-newtype (InstructionLabel l, MemLoc r) => Config l r = Config (l, Memory r)
-
-step :: (InstructionLabel l, MemLoc r) => Config l r -> Instruction l r -> Either (Memory r) (Config l r)
-step (Config (pc, mem)) Halt = Left mem
-step (Config (pc, mem)) (Inc (r,l)) = Right (Config
-  ( succ pc
-  , memInc r mem ))
-
-run :: (InstructionLabel l, MemLoc r) => Config l r -> Program l r -> Either (Memory r) (Config l r)
+run :: Config l r -> Program l -> Either (Instruction l) (Config l r)
 run = undefined -- TODO
